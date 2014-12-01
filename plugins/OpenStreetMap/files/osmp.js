5f8b722e4722477d921ef24e7da9fcc8098055d8
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-12-01 13:42:54 +0100

    updated with latest OpenStreetMap

--- plugins/OpenStreetMap/files/osmp.js
+++ plugins/OpenStreetMap/files/osmp.js
@@ -1,15 +1,149 @@
-var osmp = new Object;
-osmp.showMap = function() {
-  var map = new ol.Map({
-    target: 'osmp_map',
+var osmp = new Object();
+osmp.map = undefined;
+
+// Icon to display an issue on the map
+osmp.marker_icon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAlCAYAAAAjt+tHAAACnUlEQVRYhbWXu24TQR'
++ 'SGv1ksa21CiihConJewEKIIrIo0th9niFW8hZUvAUInsH9VimQlSJCiBeAKo1FEYyzspYZCs8xx85eZs36SKO9er5/zvl3vcc45'
++ 'wAYjUYtIALaftui2cgACywBmyRJBmCccwJvA7EfbS8gaghuvYAlkPqxTJIkM8PhUOBd4NBvY/aTgRRYAPd+u5RVxh7+3Dk3b'
++ 'Ri8EcaYgd+1QKZT33XOTc8fUl5Yy7F1jYJnkeEuiph04qkxpo8vgzZeDOwFDvg5rRyKzyJxu4wg+NnNtPS4XARopri8ltuvTw'
++ 'eFUH2+QlhUG1wGk+Pr08Gj/UoVu4QGVQlrTEBvfLEByFvh9rUqMcECeuMLTq4uH4HywHXKECRA4DKZnjRv/+xmGlyGS'
++ 'gEaHvL4FWViJwFl8LzJ8+pfVYZCASEr16ssErazBwQuzhdY2YqrztcSEPJjDdsFDjX+8yUTRZPr7ITCawkoewdsR+hrGGq8iLT'
++ 'h8swn57UInbX/FhAaWsTJ1WVzr2IdvfFF6eokQ9/ffwDKS7bTh6f2Q1n8+PhpfX+RL8xwODwCjoEj59z07a/fu2gKjnfPnsqH'
++ '6U9gJiWwJb/ZV1hYeSBTg1lk9kZUc6+ZEf/apRTgLor2IkI+y32knmlbCr4wxgwmnTjXsucPKf3sTxDsW+sJk06ce83Xf4HqC'
++ '6w/uPc39NlszSKASSf+TIAIgRtj3vhT4q/t1iwFbEhzuv6Gd87dlmVCwV+z6a3i5rSiPZetbt++5IlQ8Fc6xVsC8tvzsvDCYuDAj'
++ '65z7qsWoeAvPXzuRyqgoqgUUCDiQDIB6JXPqQEPFpAj4tCLuAWk5nNW5gqG1xKwJaLrR9tfWrJK/aIOvLYAJUJMKY+qPGLL'
++ 'OnCAv1nXlMCzka/IAAAAAElFTkSuQmCC';
+
+/*
+ * Loads the map and centers on the given position.
+ */
+osmp.showMap = function (lng, lat) {
+  this.loadMap();
+  this.setMapPosition(lng, lat);
+  this.showMarker(lng, lat);
+  this.setClickHandler();
+}
+
+/*
+ * Loads basic map functionality.
+ */
+osmp.loadMap = function (){
+  var osmp_container = 'osmp_map';
+  var osmp_layer = 'osm';
+
+  console.log("Map container: " + osmp_container);
+  osmp.map = new ol.Map({
+    target: osmp_container,
     layers: [
       new ol.layer.Tile({
-        source: new ol.source.MapQuest({layer: 'osm'})
+        source: new ol.source.MapQuest({layer: osmp_layer})
       })
-    ],
-    view: new ol.View({
-      center: ol.proj.transform([51.499778, 6.545100], 'EPSG:4326', 'EPSG:3857'),
-      zoom: 5
+    ]
+  });
+}
+
+/*
+ * Centers the map on the given position.
+ */
+osmp.setMapPosition = function (lng, lat){
+  var osmp_zoom = 17;
+  console.log("Setting map view: Lat=" + lat + ", Lng=" + lng + " zoom: " + osmp_zoom);
+  osmp.map.setView(new ol.View({
+    center: ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857'),
+    zoom: osmp_zoom
+  }));
+}
+
+/*
+ * Resolves coordinates and writes address to the document.
+ */
+osmp.getAddress = function (lng, lat){
+  console.log("Resolving coordinates: Lat=" + lat + " Lng=" + lng);
+  var link = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng+ "&language=de";
+  console.log("Running geocoder: " + link);
+  this.jsonRequest(link, function (response){
+    var address = response.results[0].formatted_address;
+    console.log("Retrieved address: " + address);
+    document.getElementById('map_address_display_text').innerHTML = address;
+  });
+}
+
+/*
+ * Resolves an address and writes coordinates to the document.
+ */
+osmp.getCoordinates = function (address){
+  console.log("Resolving address: " + address);
+  var link = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&language=de";
+  link = link.replace(/\s+/g, '+'); // Replace (multiple) spaces with plus char
+  console.log("Running geocoder: " + link);
+  this.jsonRequest(link, function(response){
+    console.log("Retrieved coordinates" + response);
+    //document.getElementById('map_coordinates_display_text').innerHTML = address;
+  });
+}
+
+/*
+ * Adds a marker to the map.
+ */
+osmp.showMarker = function(lng, lat){
+  console.log("Showing map marker Lat=" + lat + ", Lng=" + lng);
+  // Marker position
+  var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857')),
+    name: 'Ihre position',
+  });
+  // Marker style
+  var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+      opacity: 0.75,
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      graphicWidth:50,
+      graphicHeight:50,
+      // Copyright Map Icons Collection Creative Commons 3.0 BY-SA Author : Nicolas Mollet
+      src: osmp.marker_icon
     })
   });
+  // Set icon style
+  iconFeature.setStyle(iconStyle);
+
+  // Set icon(s) to vector source
+  var vectorSource = new ol.source.Vector({
+    features: [iconFeature]
+  });
+
+  // Create vector layer
+  var vectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+  });
+  // Add icon layer to map
+  osmp.map.addLayer(vectorLayer);
+}
+
+ osmp.setClickHandler = function (){
+   osmp.map.on('click', function(evt) {
+     console.log("Showing registered click");
+     var coordinate = evt.coordinate;
+     var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(coordinate, 'EPSG:4326', 'EPSG:3857'));
+     var position = ol.proj.transform(coordinate, 'EPSG:4326', 'EPSG:3857');
+     console.log("Clicked position: " + position);
+   });
+ }
+
+/*
+ * Requests a document from a given address.
+ */
+osmp.jsonRequest = function (url, callback) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var result = JSON.parse(xmlhttp.responseText);
+        if(callback){
+          callback(result);
+        }
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 }
