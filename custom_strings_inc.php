2faee532fc59d93af99aa26e493e1c56c0cbf7e1
starfish89 <raumklette@googlemail.com>
2015-01-16 11:47:19 +0100

    kleine textänderung

--- custom_strings_inc.php
+++ custom_strings_inc.php
@@ -13,6 +13,7 @@
 		$s_upload_files = 'Fotos hochladen';
 		$s_upload_file_button = 'Foto hochladen';
 		$s_upload_files_button = 'Foto hochladen';
+		$s_add_new_relationship = 'Verwandte Meldung';
 	
 	}
 	
