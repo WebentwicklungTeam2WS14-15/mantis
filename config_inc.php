c5622e013ae8392fb57948589a86aa20b3ee30a1
Bazuum <marco.horak@hsrw.org>
2015-01-19 13:49:30 +0100

    Berechtigungskonzept aufgeräumt

--- config_inc.php
+++ config_inc.php
@@ -25,28 +25,14 @@
 	#config_defaults_inc Änderungen
 	$g_allow_signup = OFF;
 	$g_admin_checks = OFF;
-	$g_view_summary_threshold       = MAJOR;
-	$g_assign_sponsored_bugs_threshold = MAJOR;
 	$g_preview_attachments_inline_max_size = 9999 * 9999;
 	$g_preview_max_width = 80;
 	$g_preview_max_height = 0;
-	$g_handle_bug_threshold                 = REPORTER;
-	$g_bug_revision_drop_threshold = MAJOR;
-	$g_upload_project_file_threshold = MAJOR;
-	$g_manage_site_threshold = MAJOR;
-	$g_manage_project_threshold = MAJOR;
-	$g_manage_news_threshold = MAJOR;
-	$g_project_user_threshold = MAJOR;
-	$g_stored_query_create_shared_threshold = MAJOR;
-	$g_update_readonly_bug_threshold = MAJOR;
+	$g_handle_bug_threshold                 = UPDATER;
 	$g_update_bug_status_threshold = REPORTER;
-	$g_set_bug_sticky_threshold = MAJOR;
-	$g_set_status_threshold = array( NEW_ => REPORTER, CLOSED => DEVELOPER );
-	$g_access_levels_enum_string = '10:viewer,25:reporter,40:updater,55:developer,70:major,90:administrator';
+	$g_set_status_threshold = array( NEW_ => REPORTER, CLOSED => MANAGER );
+	$g_access_levels_enum_string = '25:reporter,40:updater,70:manager,90:administrator';
 	$g_status_enum_string = '10:new,40:confirmed,80:resolved,90:closed';
-	$g_custom_field_link_threshold = MAJOR;
-	$g_time_tracking_reporting_threshold = MAJOR;
-	$g_manage_global_profile_threshold = MAJOR;
 	$g_my_view_boxes = array (
 		'assigned'      => '1',
 		'unassigned'    => '3',
@@ -58,5 +44,36 @@
 		'verify'		=> '0',
 		'my_comments'	=> '7'
 	);
-
+	$g_show_avatar_threshold = MANAGER;
+	$g_private_news_threshold	= MANAGER;
+	$g_reminder_receive_threshold = MANAGER;
+	$g_handle_sponsored_bugs_threshold = MANAGER;
+	$g_delete_attachments_threshold	= MANAGER;
+	$g_monitor_add_others_bug_threshold = MANAGER;
+	$g_monitor_delete_others_bug_threshold = MANAGER;
+	$g_private_bug_threshold		= MANAGER;
+	$g_private_bugnote_threshold	= MANAGER;
+	$g_bug_reminder_threshold		= MANAGER;
+	$g_update_bugnote_threshold = MANAGER;
+	$g_delete_bug_threshold = MANAGER;
+	$g_move_bug_threshold = MANAGER;
+	$g_show_monitor_list_threshold = MANAGER; 
+	$g_stored_query_create_threshold = MANAGER;
+	$g_roadmap_update_threshold = MANAGER;
+	$g_reopen_bug_threshold			= MANAGER;
+	$g_report_issues_for_unreleased_versions_threshold = MANAGER;
+	$g_development_team_threshold = MANAGER;
+	$g_create_permalink_threshold = MANAGER;
+	$g_tag_detach_threshold = MANAGER;
+	$g_tag_edit_threshold = MANAGER;
+	$g_time_tracking_view_threshold = MANAGER;
+	$g_time_tracking_edit_threshold = MANAGER;
+	$g_view_attachments_threshold	= UPDATER;
+	$g_download_attachments_threshold	= UPDATER;
+	$g_view_handler_threshold		= UPDATER;
+	$g_view_history_threshold		= UPDATER;
+	$g_view_changelog_threshold = UPDATER;
+	$g_roadmap_view_threshold = UPDATER;
+	$g_tag_view_threshold = UPDATER;
+	
 ?>
